0037e79ec0daa2414f40845093954a2580048452
Tushar Saha <sahatushar242002@gmail.com>
2025-10-19 20:10:44 +0530

    added instructor section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 
@@ -11,6 +13,8 @@ export default function Home() {
       <FeaturedCourses />
       <WhyChooseUs />
       <MusicSchoolTestimonials />
+      <UpcomingWebinars />
+      <Instructors />
     </main>
   );
 }
--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -20,7 +20,7 @@ function FeaturedCourses() {
 
     return (
         <div className="py-12 bg-gray-900">
-            <div>
+            <div className="max-w-7xl mx-auto px-4 sm:px-6">
                 <div className="text-center">
                     <h2 className="text-base text-teal-600 font-semibold tracking-wide uppercase">FEATURED COURSES</h2>
                     <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">Learn with the Best</p>
